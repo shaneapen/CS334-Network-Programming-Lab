3abf929df45fb89d5a68d8f704f0e8974c1e8a4a
hemanth <hemanthwarrier@gmail.com>
2019-04-15 22:05:01 +0530

    udp error corrected

--- tcp/client.py
+++ tcp/client.py
@@ -4,13 +4,13 @@ TCP_IP="127.0.0.1"
 TCP_PORT=5191
 BUFFER_SIZE=1
 
-MSG1 =raw_input("Enter 1st number")
-MSG2 =raw_input("Enter 2nd number")
+MSG1 =input("Enter 1st number: ")
+MSG2 =input("Enter 2nd number: ")
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((TCP_IP,TCP_PORT))
-s.send(MSG1)
-s.send(MSG2)
-data = s.recv(2)
+s.send((MSG1.encode()))
+s.send((MSG2.encode()))
+data = s.recv(2).decode()
 s.close()
-print "sum is:",data
+print (("sum is:",data))
--- tcp/server.py
+++ tcp/server.py
@@ -2,16 +2,17 @@ import socket
 
 TCP_IP="127.0.0.1"
 TCP_PORT=5191
-BUFFER_SIZE=1
+BUFFER_SIZE=1024
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((TCP_IP,TCP_PORT))
 s.listen(1)
 conn,addr=s.accept()
-print 'connection address:',addr
-data1 =conn.recv(BUFFER_SIZE)
-data2 =conn.recv(BUFFER_SIZE)
+print (('connection address:',addr))
+data1 =conn.recv(BUFFER_SIZE).decode()
+data2 =conn.recv(BUFFER_SIZE).decode()
+print(data1,data2)
 data =int(data1)+int(data2)
-conn.send(str(data))
+print(data)
+conn.send((data.encode()))
 conn.close()
-
