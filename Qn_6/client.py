d986b7aab8b6d3facff7743a09ceb485cd219518
Shan Eapen Koshy <shaneapen@gmail.com>
2019-04-29 21:18:37 +0530

    removed pickle

--- Qn_6/client.py
+++ Qn_6/client.py
@@ -1,14 +1,14 @@
-import socket,pickle
+import socket
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5012
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
+
 num1 = input("Enter 1st number:")
 num2 = input("Enter 2st number:")
 
+num = "\n".join([num1,num2]).encode()
 
-num = [num1, num2]
-MESSAGE = pickle.dumps({"a":num})
+sock.sendto(num, (UDP_IP, UDP_PORT))
+data,addr = sock.recvfrom(1024)
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-data,addr =sock.recvfrom(1024)
 print ("Sum is",data.decode())
--- Qn_6/server.py
+++ Qn_6/server.py
@@ -1,4 +1,4 @@
-import socket,pickle
+import socket
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5012
@@ -9,7 +9,7 @@ sock.bind((UDP_IP, UDP_PORT))
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print ("Data received from", addr)
-    num=pickle.loads(data)
-    arr = num.get("a")
-    s=int(arr[0])+int(arr[1])
+
+    num = data.decode().split("\n")
+    s=int(num[0])+int(num[1])
     sock.sendto(str(s).encode(), (addr[0], addr[1]))
